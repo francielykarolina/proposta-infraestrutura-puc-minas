36272d13ad6c32554f2f0568fc3350b71ac56d9e
Franciely Andrade <franciely.karolina@gmail.com>
2022-06-22 20:02:22 -0300

    Adicionado menu

--- Program.cs
+++ Program.cs
@@ -7,12 +7,36 @@ namespace PropostaInfraestrutura
     {
         static void Main(string[] args)
         {
-                        
-            Paciente paciente = new Paciente();
-            paciente.PreencherDados();
-            Console.Clear();
-            paciente.Imprimir();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Bem Vindo (a)! Escolha uma das opções abaixo");
+                Console.WriteLine("1 - Cadastrar novo paciente");
+                Console.WriteLine("2 - Pesquisar paciente");
+                Console.WriteLine("0 - Sair");
+                int op = int.Parse(Console.ReadLine());
 
+                if (op == 0) 
+                {
+                    break;
+                }
+                else if (op == 1) 
+                {
+                    Paciente paciente = new Paciente();
+                    paciente.PreencherDados();
+                    Console.Clear();
+                }
+                else if(op == 2)
+                {
+                    
+                }
+
+            }
+
+
+
+            
+            
         }
     }
 }
